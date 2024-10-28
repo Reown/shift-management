608844a6440bea4e95df02f12eef40aff5035c17
joey <joey@chow.cc>
2024-10-29 01:05:36 +0800

    init

---
+++ server-express/src/server.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import taskRoutes from "./routes/tasks.route";
+import oldPersonRoutes from "./routes/oldperson.route";
+import testpkRoutes from "./routes/testpk.route";
+import personRoutes from "./routes/person.route";
+
+const main = async (): Promise<void> => {
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+
+  const port = process.env.DB_PORT;
+
+  app.use("/api/task", taskRoutes);
+  app.use("/api/oldperson", oldPersonRoutes);
+  app.use("/api/testpk", testpkRoutes);
+  app.use("/api/person", personRoutes);
+
+  //app.get("/", (req: Request, res: Response) => {
+  //res.send("Hello, Ttestdss!");
+  //});
+
+  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    res.status(500).send("Something went wrong");
+  });
+
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
