124343e57fd3bcc3ce827c3ec8b60a8fd8b63f09
joey <joey@chow.cc>
2024-10-30 01:17:27 +0800

    formatting & parent context

--- client-react/src/pages/SignupAcc.tsx
+++ client-react/src/pages/SignupAcc.tsx
@@ -1,59 +1,80 @@
-import { useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 
-const SignupAcc = () => {
+interface SignUpAccContextType {
+  isDisabled: boolean;
+  setIsDisabled: (disabled: boolean) => void;
+}
+
+export const ParentContext = createContext<SignUpAccContextType>({
+  isDisabled: false,
+  setIsDisabled: () => {},
+});
+
+const SignUpAcc = () => {
   useEffect(() => {});
 
   const navigate = useNavigate();
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [cfmPassword, setCfmPassword] = useState<string>("");
+  const [isDisabled, setIsDisabled] = useState(false);
 
   const clickContinue = (item: string[]) => {
+    console.log(item);
     navigate("/signup/details");
   };
 
   return (
-    <div className="container">
-      <div className="card loginblock mx-auto box">
-        <div className="card-body">
-          <form className="row g-3 formbody">
-            <label>Username</label>
-            <input
-              type="text"
-              className="form-control textarea"
-              id="username"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control textarea"
-              id="password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <label>Confirm Password</label>
-            <input
-              type="password"
-              className="form-control textarea"
-              id="cfmpassword"
-              onChange={(e) => setCfmPassword(e.target.value)}
-            />
-            <button
-              type="button"
-              onClick={() => {
-                clickContinue([username]);
-              }}
-              className="btn"
-            >
-              Continue
-            </button>
-          </form>
+    <ParentContext.Provider value={{ isDisabled, setIsDisabled }}>
+      <div className="container">
+        <div
+          className={
+            isDisabled
+              ? "disabled card loginblock mx-auto item1"
+              : "card loginblock mx-auto item1"
+          }
+        >
+          <div className="card-body">
+            <form className="row g-3 formbody">
+              <label>Username</label>
+              <input
+                type="text"
+                className="form-control textarea"
+                id="username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control textarea"
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <label>Confirm Password</label>
+              <input
+                type="password"
+                className="form-control textarea"
+                id="cfmpassword"
+                onChange={(e) => setCfmPassword(e.target.value)}
+              />
+              <button
+                type="button"
+                onClick={() => {
+                  clickContinue([username]);
+                }}
+                className="btn"
+              >
+                Continue
+              </button>
+            </form>
+          </div>
         </div>
+
+        <Outlet />
       </div>
-      <Outlet />
-    </div>
+    </ParentContext.Provider>
   );
 };
 
-export default SignupAcc;
+export default SignUpAcc;
--- client-react/src/pages/SignupDet.tsx
+++ client-react/src/pages/SignupDet.tsx
@@ -1,16 +1,43 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { ParentContext } from "./SignupAcc";
 
-const SignupDet = () => {
-  const [age, setAge] = useState<string>("");
+const SignUpDet = () => {
+  const { setIsDisabled } = useContext(ParentContext);
+  useEffect(() => {
+    setIsDisabled(true);
+    return () => {
+      setIsDisabled(false);
+    };
+  });
   const [email, setEmail] = useState<string>("");
 
   return (
-    <div className="card loginblock mx-auto box">
+    <div className="card loginblock mx-auto item2">
       <div className="card-body">
         <form className="row g-3 formbody">
-          <label>Age</label>
-          <input type="text" className="form-control textarea" id="username" />
+          <div className="col-md-6">
+            <label>First Name</label>
+            <input
+              type="text"
+              className="form-control textarea"
+              id="username"
+            />
+          </div>
+          <div className="col-md-6">
+            <label>Last Name</label>
+            <input
+              type="password"
+              className="form-control textarea"
+              id="password"
+            />
+          </div>
           <label>Email</label>
+          <input
+            type="password"
+            className="form-control textarea"
+            id="password"
+          />
+          <label>Date of Birth</label>
           <input
             type="password"
             className="form-control textarea"
@@ -25,4 +52,6 @@ const SignupDet = () => {
   );
 };
 
-export default SignupDet;
+export default SignUpDet;
+
+// role, occupation
