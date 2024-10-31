5e2e9e28563b6b57d5a1e2e4f5c227e3aae240ee
joey <joey@chow.cc>
2024-10-31 22:19:08 +0800

    password helper

---
+++ server-express/src/helpers/password.helper.ts
@@ -0,0 +1,22 @@
+import { genSalt, hash, compare } from "bcryptjs";
+
+export const bcrypt = async (password: string) => {
+  return await hash(password, await genSalt(10));
+};
+
+export const match = async (password: string, input: string) => {
+  return await compare(password, input);
+};
+
+export const genRandomPw = (length: number) => {
+  const charset =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~";
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+
+  return password;
+};
