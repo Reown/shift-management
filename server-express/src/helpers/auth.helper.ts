26da6bebddbffa8998fa54f08bf751328077d0ae
joey <joey@chow.cc>
2024-11-01 00:58:51 +0800

    removed logs

--- server-express/src/helpers/auth.helper.ts
+++ server-express/src/helpers/auth.helper.ts
@@ -1,12 +1,10 @@
 import jwt from "jsonwebtoken";
 
 export const newToken = (email: string, role: string) => {
-  console.log("test" + email);
   const token = jwt.sign(
     { email: email, role: role },
     process.env.JWT_SECRET as string,
     { expiresIn: "1h" }
   );
-  console.log(token);
   return token;
 };
