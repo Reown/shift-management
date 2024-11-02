672a268479e23e7ab8bb539d591820b5a0611294
joey <joey@chow.cc>
2024-11-03 03:41:02 +0800

    fixed dynamic get cookie

--- client-react/src/config/header.ts
+++ client-react/src/config/header.ts
@@ -1,5 +1,7 @@
 import Cookies from "js-cookie";
 
-export const tokenHeader = {
-  headers: { Authorization: `Bearer ${Cookies.get("token") as string}` },
+export const tokenHeader = () => {
+  return {
+    headers: { Authorization: `Bearer ${Cookies.get("token") as string}` },
+  };
 };
