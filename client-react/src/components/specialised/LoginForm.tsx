ad33f338a36c6d0b9691e0dfa2401e71b5e1880d
joey <joey@chow.cc>
2024-11-04 00:25:33 +0800

    moved form to specialised components

---
+++ client-react/src/components/specialised/LoginForm.tsx
@@ -0,0 +1,40 @@
+import { FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { LabeledTextField } from "../LabeledTextField";
+import { login } from "../../services/Person";
+import { createToken } from "../../services/Auth";
+
+const LoginForm = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const clickLogin = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const res = await login([email, password]);
+    if (res) {
+      const token = await createToken(email);
+      if (token) {
+        navigate(res);
+      }
+    }
+  };
+
+  return (
+    <form className="row g-3 formbody" onSubmit={clickLogin}>
+      <LabeledTextField
+        children={["Email", "text"]}
+        onChange={(e) => setEmail(e)}
+      ></LabeledTextField>
+      <LabeledTextField
+        children={["Password", "password"]}
+        onChange={(e) => setPassword(e)}
+      ></LabeledTextField>
+      <button type="submit" className="btn">
+        Login
+      </button>
+    </form>
+  );
+};
+
+export default LoginForm;
--- client-react/src/pages/Login.tsx
+++ client-react/src/pages/Login.tsx
@@ -1,8 +1,5 @@
-import { FormEvent, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { login } from "../services/Person";
-import { createToken } from "../services/Auth";
-import { LabeledTextField } from "../components/LabeledTextField";
+import { useEffect } from "react";
+import LoginForm from "../components/specialised/LoginForm";
 import Cookies from "js-cookie";
 
 const Login = () => {
@@ -10,39 +7,10 @@ const Login = () => {
     Cookies.remove("token");
   });
 
-  const navigate = useNavigate();
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-
-  const clickLogin = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const res = await login([email, password]);
-    if (res) {
-      const token = await createToken(email);
-      if (token) {
-        navigate(res);
-      }
-    }
-  };
-
   return (
-    <div>
-      <div className="card loginblock mx-auto">
-        <div className="card-body">
-          <form className="row g-3 formbody" onSubmit={clickLogin}>
-            <LabeledTextField
-              children={["Email", "text"]}
-              onChange={(e) => setEmail(e)}
-            ></LabeledTextField>
-            <LabeledTextField
-              children={["Password", "password"]}
-              onChange={(e) => setPassword(e)}
-            ></LabeledTextField>
-            <button type="submit" className="btn">
-              Login
-            </button>
-          </form>
-        </div>
+    <div className="card loginblock mx-auto">
+      <div className="card-body">
+        <LoginForm></LoginForm>
       </div>
     </div>
   );
