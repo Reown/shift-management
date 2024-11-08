0ea86412bfebeea73c9f5748a414e146d6348a07
joey <joey@chow.cc>
2024-11-09 01:28:22 +0800

    comps > pages

--- client-react/src/pages/Dashboard.tsx
+++ client-react/src/pages/Dashboard.tsx
@@ -1,15 +1,27 @@
-import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import TokenRole from "../hooks/TokenRole";
+import AdminDashboard from "./AdminDashboard";
+import ManagerDashboard from "./ManagerDashboard";
+import UserDashboard from "./UserDashboard";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
   const role = TokenRole();
 
-  return (
-    <div>
-      test
-      <p>test2 {role}</p>
-    </div>
-  );
+  const renderDashboard = (role: any) => {
+    switch (role) {
+      case "admin":
+        return <AdminDashboard />;
+      case "manager":
+        return <ManagerDashboard />;
+      case "user":
+        return <UserDashboard />;
+      default:
+        navigate("/login");
+    }
+  };
+
+  return <div> {renderDashboard(role)} </div>;
 };
 
 export default Dashboard;
